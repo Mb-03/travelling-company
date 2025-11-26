49c5d88af418649bd5f997a249a7ba34f277e6c7
Mb-03 <baratashvilimisho@gmail.com>
2025-11-27 01:47:43 +0400

    Initialize Next.js project structure
    Set up a new Next.js app with basic configuration, global styles, initial layout, and landing/header feature components. Added essential config files for ESLint, PostCSS, TypeScript, and Next.js, as well as public assets and README documentation.

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import LandingPage from "@/features/landing/components/LandingPage"
+
+
+const Home = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  )
+}
+
+export default Home
