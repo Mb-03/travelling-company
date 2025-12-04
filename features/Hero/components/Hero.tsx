2e18271f681c561aeec86a3e7c8a623e4dab04d7
Sandro sulava <sandrosulava2@gmail.com>
2025-12-05 01:19:44 +0400

    Revamp Hero component layout and add Boarding.svg
    Updated the Hero component with improved layout, styling, and Next.js Image usage. Added a new Boarding.svg asset for enhanced visual presentation.

--- features/Hero/components/Hero.tsx
+++ features/Hero/components/Hero.tsx
@@ -1,14 +1,33 @@
-import "../global.css";
+"use client";
+
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="">
-      <h2 className="bg-[var(--foreground)]  text-[64px] leading-[66px] font text-[#4E4B66] max-w-[669px] tracking-[1px]">
-        Plan your next big trip overseas.
-      </h2>
-      <div>
-        <button>BEGIN PLANS</button>
-        <button>REVIEW PLANS</button>
+    <div className="pt-60 pl-[135px]">
+      <div className="relative">
+        <div>
+          <h2 className="text-[64px] leading-[66px] font text-[#4E4B66] max-w-[669px] tracking-[1px]">
+            Plan your next big trip overseas.
+          </h2>
+        </div>
+        <div className="flex gap-[25px] pt-[25px]">
+          <button className="w-[243px] h-16 rounded-[40px] bg-[#5F2EEA]">
+            BEGIN PLANS
+          </button>
+          <button className="w-[243px] h-16 rounded-[40px] bg-white border-2 border-[#5F2EEA] text-[#5F2EEA]">
+            REVIEW PLANS
+          </button>
+        </div>
+      </div>
+
+      <div className="absolute left-[454px] top-0 ">
+        <Image
+          src="/Boarding.svg"
+          alt="Boarding Icon"
+          width={1090}
+          height={634}
+        />
       </div>
     </div>
   );
