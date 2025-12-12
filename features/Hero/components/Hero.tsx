faa142498d7857b2c013987287cc3a6ef2fad943
Sandro sulava <sandrosulava2@gmail.com>
2025-12-12 20:16:48 +0400

    bugfix
    made some changes

--- features/Hero/components/Hero.tsx
+++ features/Hero/components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="pt-60 pl-[135px]">
+    <div className="pt-60 pl-[135px] ">
       <div className="relative">
         <div>
           <h2 className="text-[64px] leading-[66px] font text-[#4E4B66] max-w-[669px] tracking-[1px]">
@@ -12,7 +12,7 @@ const Hero = () => {
           </h2>
         </div>
         <div className="flex gap-[25px] pt-[25px]">
-          <button className="w-[243px] h-16 rounded-[40px] bg-[#5F2EEA]">
+          <button className="w-[243px] h-16 rounded-[40px] bg-[#5F2EEA] border-none">
             BEGIN PLANS
           </button>
           <button className="w-[243px] h-16 rounded-[40px] bg-white border-2 border-[#5F2EEA] text-[#5F2EEA]">
@@ -25,7 +25,7 @@ const Hero = () => {
         <Image
           src="/Boarding.svg"
           alt="Boarding Icon"
-          width={1090}
+          width={1409}
           height={634}
         />
       </div>
